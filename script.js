ec20b92bbd0a1c84eea334f4a57972f6e34a19b1
LQuesadaM <luisquesada163@gmail.com>
2024-04-03 07:25:39 -0300

    🍻 retrieve the elements #message-input, #result, and #check-message-btn from the HTML document, and assign to the variables messageInput, result, and checkMessageButton

---
+++ script.js
@@ -0,0 +1,3 @@
+const messageInput = document.getElementById("message-input");
+const checkMessageButton = document.getElementById("check-message-btn");
+const result = document.getElementById("result");
