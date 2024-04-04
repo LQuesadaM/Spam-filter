24f7019afdda7b4dcd52e58edd306f7f53c3b6f6
LQuesadaM <luisquesada163@gmail.com>
2024-04-04 12:51:12 -0300

    🍻 update freeRegex expression

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const result = document.getElementById("result");
 //regex
 const helpRegex = /please help|assist me/i;
 const dollarRegex = /[0-9]+ (?:hundred|thousand|million|billion) dollars/i;
-const freeRegex = /fr[e3]+ m[o0]n[e3]y/i;
+const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i;
 
 const denyList = [helpRegex, dollarRegex, freeRegex];
 
