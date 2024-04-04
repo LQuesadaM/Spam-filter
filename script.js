e6345aea63af45d32f52e507d6287a4631bb2c26
LQuesadaM <luisquesada163@gmail.com>
2024-04-04 15:49:14 -0300

    💡 added comments to the meaning of regex const

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ const result = document.getElementById("result");
 //regex
 const helpRegex = /please help|assist me/i;
 const dollarRegex = /[0-9]+ (?:hundred|thousand|million|billion) dollars/i;
-const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i;
+const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i; //free money
 const stockRegex = /(?:^|\s)[s5][t7][o0][c{[(]k [a@4]l[e3]r[t7](?:$|\s)/i; // stock alert
+
 
 const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex];
 
